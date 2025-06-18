a75de87fe04fc15e6da159e557dfed89620872af
eduar-darin <167717646+eduar-darin@users.noreply.github.com>
2025-06-17 19:00:40 -0500

    init login creation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { LoginModule } from './login/login.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [LoginModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
